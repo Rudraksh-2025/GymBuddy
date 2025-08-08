ccb3fbbc51531d9823ee4e06bb5d515cd9720643
Rudraksh-2025 <rud.crestcoder@gmail.com>
2025-08-08 11:39:59 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () =>
+            console.log(`Server running on port ${process.env.PORT}`)
+        );
+    })
+    .catch(err => console.error(err));
