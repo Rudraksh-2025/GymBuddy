b2332a906e6a65336df5e23dd8a21d31b69aff97
Rudraksh-2025 <rud.crestcoder@gmail.com>
2026-03-11 16:31:11 +0530

    issue fix

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: [
-        "https://gymbuddy1310.netlify.app",
-        "http://localhost:3001"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: true,
     credentials: true
 }));
 
@@ -50,7 +45,7 @@ const PORT = process.env.PORT || 4000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Mongo connected');
-        // app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+        app.listen(PORT, () => console.log(`Server started on ${PORT}`));
     })
     .catch(err => {
         console.error('DB connection error', err);
