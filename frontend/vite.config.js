9c21dda27894c997338b6e20a0a61140b41a508f
Rudraksh-2025 <rud.crestcoder@gmail.com>
2026-03-11 14:55:21 +0530

    pwa plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My React App',
+        short_name: 'ReactApp',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/weightlifter.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/weightlifter.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
-    host: '0.0.0.0', // listen on all addresses, not just localhost
-    port: 3001,      // or any port you want
+    host: '0.0.0.0',
+    port: 3001,
   },
 })
